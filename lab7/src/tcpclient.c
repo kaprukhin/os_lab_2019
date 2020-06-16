ed5b5b9768e889ff9e2559260253ff59913939e2
kaprukhin <evgeny1388@gmail.com>
2020-06-16 10:17:08 +0000

    done

--- lab7/src/tcpclient.c
+++ lab7/src/tcpclient.c
@@ -7,21 +7,81 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define BUFSIZE 100
+#include <getopt.h>
+
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 #define SIZE sizeof(struct sockaddr_in)
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv)
+{
+  int BUFSIZE = -1;
+	int SERV_PORT = -1;
+	char* ADDR;
+	while (1)
+  {
+	  int current_optind = optind ? optind : 1;
+
+	  static struct option options[] = {{"BUFSIZE", required_argument, 0, 0},
+	                                    {"SERV_PORT", required_argument, 0, 0},
+	                                    {"ADDR", required_argument, 0, 0},
+	                                    {0, 0, 0, 0}};
+	
+	  int option_index = 0;
+	  int c = getopt_long(argc, argv, "", options, &option_index);
+
+	  if (c == -1) break;
+		switch (c)
+    {
+	    case 0:
+	      switch (option_index)
+        {
+	        case 0:
+	          BUFSIZE = atoi(optarg);
+	          if (BUFSIZE <= 0)
+            {
+	            printf("BUFSIZE must be a positive number\n");
+	            return 1;
+	          }
+	          break;
+	        case 1:
+	          SERV_PORT = atoi(optarg);
+	          if (SERV_PORT <= 0)
+            {
+	            printf("SERV_PORT must be a positive number\n");
+	            return 1;
+	          }
+	          break;
+	        case 2:
+	          ADDR = optarg;
+	          break;
+	      }
+	    case '?':
+	      break;
+	
+	    default:
+	      printf("getopt returned character code 0%o?\n", c);
+	  }
+	
+	}
+	if (BUFSIZE == -1 || SERV_PORT == -1)
+  {
+	  printf("Usage: %s --bufsize \"buffer_size\" --serv_port \"serv_port\"\n", argv[0]);
+	  return 1;
+	}
+  
   int fd;
   int nread;
   char buf[BUFSIZE];
   struct sockaddr_in servaddr;
-  if (argc < 3) {
+  if (argc < 3)
+  {
     printf("Too few arguments \n");
     exit(1);
   }
 
-  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
     perror("socket creating");
     exit(1);
   }
@@ -29,21 +89,25 @@ int main(int argc, char *argv[]) {
   memset(&servaddr, 0, SIZE);
   servaddr.sin_family = AF_INET;
 
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, ADDR, &servaddr.sin_addr) <= 0)
+  {
     perror("bad address");
     exit(1);
   }
 
-  servaddr.sin_port = htons(atoi(argv[2]));
+  servaddr.sin_port = htons(SERV_PORT);
 
-  if (connect(fd, (SADDR *)&servaddr, SIZE) < 0) {
+  if (connect(fd, (SADDR *)&servaddr, SIZE) < 0)
+  {
     perror("connect");
     exit(1);
   }
 
   write(1, "Input message to send\n", 22);
-  while ((nread = read(0, buf, BUFSIZE)) > 0) {
-    if (write(fd, buf, nread) < 0) {
+  while ((nread = read(0, buf, BUFSIZE)) > 0)
+  {
+    if (write(fd, buf, nread) < 0)
+    {
       perror("write");
       exit(1);
     }
--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -7,11 +7,68 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
+#include <getopt.h>
+
+//#define SERV_PORT 10050
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
+int main(int argc, char **argv)
+{
+  
+  int SERV_PORT = -1;
+  int BUFSIZE = -1;
+
+  while (1)
+  {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"BUFSIZE", required_argument, 0, 0},
+                                      {"SERV_PORT", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+    
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index); 
+        
+    if (c == -1) break;
+    
+    switch (c)
+    {
+      case 0:
+        switch (option_index)
+        {
+          case 0:
+            BUFSIZE = atoi(optarg);
+            if (BUFSIZE < 1)
+            {
+              BUFSIZE=-1;
+              return 1;
+            }
+            break;
+          case 1:
+            SERV_PORT = atoi(optarg);
+            if (SERV_PORT < 0)
+            { 
+              SERV_PORT=-1;
+              return 1;
+            }
+            break;
+          defalut:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+      case '?':
+        break; 
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
+  if (BUFSIZE == -1 || SERV_PORT == -1)
+  {
+    printf("Usage: %s --BUFSIZE 100 --SERV_PORT 10050 \n", argv[0]);
+    return 1;
+  }
+  
   const size_t kSize = sizeof(struct sockaddr_in);
 
   int lfd, cfd;
@@ -20,7 +77,8 @@ int main() {
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
-  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
     perror("socket");
     exit(1);
   }
@@ -30,30 +88,36 @@ int main() {
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(SERV_PORT);
 
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
+  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0)
+  {
     perror("bind");
     exit(1);
   }
 
-  if (listen(lfd, 5) < 0) {
+  if (listen(lfd, 5) < 0)
+  {
     perror("listen");
     exit(1);
   }
 
-  while (1) {
+  while (1)
+  {
     unsigned int clilen = kSize;
 
-    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
+    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0)
+    {
       perror("accept");
       exit(1);
     }
     printf("connection established\n");
 
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
+    while ((nread = read(cfd, buf, BUFSIZE)) > 0)
+    {
       write(1, &buf, nread);
     }
 
-    if (nread == -1) {
+    if (nread == -1)
+    {
       perror("read");
       exit(1);
     }
